d8dc88ee4de5a848fcc5e4b4ef0c62b8a2a47792
bbrument <brument.bcb@gmail.com>
2025-06-06 17:22:33 +0200

    test

--- run_os.sh
+++ run_os.sh
@@ -10,17 +10,18 @@ mkdir -p $OUTPUT_PATH
 views=(23 22 21 20 19 18 17 16 14 13 12 11 10 9 8 7 6 5 4 3 2 1)
 for view in "${views[@]}"; do
 
-    pad_ind=$(printf "%08d" $(($view - 1)))
-    if [ ! -d "$DATA_PATH/view_${pad_ind}" ]; then
+    pad_view=$(printf "%02d" $view)
+    if [ ! -d "$DATA_PATH/view_${pad_view}" ]; then
         echo "Directory for view $view does not exist, skipping."
         continue
     fi
+
+    pad_ind=$(printf "%08d" $(($view - 1)))
     if [ -f "$OUTPUT_PATH/${pad_ind}.png" ]; then
         echo "Output file for view $view already exists, skipping."
         continue
     fi
 
-    pad_view=$(printf "%02d" $view)
     if [ -f "$OUTPUT_PATH/view_${pad_view}.png" ]; then
         echo "Temporary file for view $view already exists, skipping."
         mv "$OUTPUT_PATH/view_${pad_view}.png" "$OUTPUT_PATH/${pad_ind}.png"
