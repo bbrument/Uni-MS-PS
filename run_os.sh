f85f22f1ae57ad585d90210f7bbc2627245d5102
bbrument <brument.bcb@gmail.com>
2025-06-06 17:20:37 +0200

    test

--- run_os.sh
+++ run_os.sh
@@ -11,11 +11,11 @@ views=(23 22 21 20 19 18 17 16 14 13 12 11 10 9 8 7 6 5 4 3 2 1)
 for view in "${views[@]}"; do
 
     pad_ind=$(printf "%08d" $(($view - 1)))
-    if [ ! -d "$DATA_PATH/view_$pad_ind" ]; then
+    if [ ! -d "$DATA_PATH/view_${pad_ind}" ]; then
         echo "Directory for view $view does not exist, skipping."
         continue
     fi
-    if [ -f "$OUTPUT_PATH/$pad_ind.png" ]; then
+    if [ -f "$OUTPUT_PATH/${pad_ind}.png" ]; then
         echo "Output file for view $view already exists, skipping."
         continue
     fi
@@ -23,7 +23,7 @@ for view in "${views[@]}"; do
     pad_view=$(printf "%02d" $view)
     if [ -f "$OUTPUT_PATH/view_${pad_view}.png" ]; then
         echo "Temporary file for view $view already exists, skipping."
-        mv "$OUTPUT_PATH/view_${pad_view}.png" "$OUTPUT_PATH/$pad_ind.png"
+        mv "$OUTPUT_PATH/view_${pad_view}.png" "$OUTPUT_PATH/${pad_ind}.png"
         continue
     fi
 
@@ -33,5 +33,5 @@ for view in "${views[@]}"; do
         --path_obj "$DATA_PATH/view_${pad_view}" \
         --cuda
 
-    mv "$OUTPUT_PATH/view_${pad_view}.png" "$OUTPUT_PATH/$pad_ind.png"
+    mv "$OUTPUT_PATH/view_${pad_view}.png" "$OUTPUT_PATH/${pad_ind}.png"
 done
